0909bf8807bd1821ebccd0ad07ac790dd0ed1f91
Chris Montgomery <christopher.montgomery@dowjones.com>
2014-07-06 18:20:46 -0700

    finish example

--- examples/simple/bundle.config.js
+++ examples/simple/bundle.config.js
@@ -8,7 +8,7 @@ module.exports = {
       styles: [
         './content/**/*.css'
       ],
-      resources: './content/**/*.{png}'
+      resources: './content/**/*.{png,svg}'
     }
   },
   dest: './public'
