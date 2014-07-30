4dc79aeec9815d24f0d6df919236ea80579f4c9b
Chris Montgomery <christopher.montgomery@dowjones.com>
2014-07-30 12:17:24 -0500

    add example for how generating all environment's bundles would work

---
+++ examples/both-min-and-unmin/bundle.config.js
@@ -0,0 +1,32 @@
+var prodLikeEnvs = ['production', 'staging']; // when NODE_ENV=staging or NODE_ENV=production
+module.exports = {
+  bundle: {
+    vendor: {
+      scripts: [
+        {src: './bower_components/jquery/jquery.js', minSrc: './bower_components/jquery/jquery.min.js'},
+        {src: './bower_components/angular/angular.js', minSrc: './bower_components/angular/angular.min.js'},
+        './bower_components/spin/spin.js'
+      ],
+      styles: {
+        src: './bower_components/bootstrap/dist/css/bootstrap.css',
+        minSrc: './bower_components/bootstrap/dist/css/bootstrap.min.css'
+      },
+      options: {
+        useMin: prodLikeEnvs,
+        uglify: false,
+        rev: prodLikeEnvs
+      }
+    },
+    main: {
+      scripts: './content/**/*.js',
+      styles: './content/**/*.css',
+      options: {
+        uglify: prodLikeEnvs,
+        rev: prodLikeEnvs
+      }
+    }
+  },
+  options: {
+    bundleAllEnvironments: true // boolean|string|array (e.g. true, false, 'production', ['production', 'staging'])
+  }
+};
---
+++ examples/both-min-and-unmin/gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp'),
+  rimraf = require('gulp-rimraf'),
+  bundle = require('../../index');
+
+gulp.task('bundle', ['clean'], function () {
+  return gulp.src('./bundle.config.js')
+    .pipe(bundle())
+    .pipe(bundle.results({
+      dest: './',
+      pathPrefix: '/public/'
+    }))
+    .pipe(gulp.dest('./public'));
+});
+
+gulp.task('clean', function () {
+  return gulp.src('./public', { read: false })
+    .pipe(rimraf());
+});
