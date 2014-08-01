21ad333baec0d7535c10b5a958bc7cafe368c511
Chris Montgomery <christopher.montgomery@dowjones.com>
2014-08-01 14:20:33 -0500

    add ability for multiple result bundles

---
+++ test/unit/add-bundle-results-test.js
@@ -0,0 +1,101 @@
+'use strict';
+var libPath = './../../lib',
+  addBundleResults = require(libPath + '/add-bundle-results'),
+  Bundle = require(libPath + '/model/bundle'),
+  BundleType = require(libPath + '/model/bundle-type'),
+  should = require('should'),
+  File = require('vinyl');
+
+describe('add-bundle-results', function () {
+  it('should return result obj with one new entry', function () {
+    var file = new File({
+      base: '/app/public',
+      path: '/app/public/main-bundle.js'
+    });
+    file.bundle = new Bundle({
+      name: 'main',
+      type: BundleType.SCRIPTS
+    });
+    var expected = {
+      default: {
+        contents: {
+          main: {
+            scripts: "<script src='main-bundle.js' type='text/javascript'></script>"
+          }
+        },
+        filename: 'bundle.result.json'
+      }
+    };
+    addBundleResults({}, file).should.eql(expected);
+  });
+
+  it('should return result obj new bundle type appended', function () {
+    var currentBundleResults = {
+      "default": {
+        "contents": {
+          main: {
+            scripts: "<script src='main-bundle.js' type='text/javascript'></script>"
+          }
+        },
+        filename: 'bundle.result.json'
+      }
+    };
+    var file = new File({
+      base: '/app/public',
+      path: '/app/public/main-bundle.css'
+    });
+    file.bundle = new Bundle({
+      name: 'main',
+      type: BundleType.STYLES
+    });
+    var expected = {
+      default: {
+        contents: {
+          main: {
+            scripts: "<script src='main-bundle.js' type='text/javascript'></script>",
+            styles: "<link href='main-bundle.css' media='screen' rel='stylesheet' type='text/css'/>"
+          }
+        },
+        filename: 'bundle.result.json'
+      }
+    };
+    addBundleResults(currentBundleResults, file).should.eql(expected);
+  });
+
+  it('should return result obj new bundle appended', function () {
+    var currentBundleResults = {
+      "default": {
+        "contents": {
+          main: {
+            scripts: "<script src='main-bundle.js' type='text/javascript'></script>",
+            styles: "<link href='main-bundle.css' media='screen' rel='stylesheet' type='text/css'/>"
+          }
+        },
+        filename: 'bundle.result.json'
+      }
+    };
+    var file = new File({
+      base: '/app/public',
+      path: '/app/public/vendor-bundle.js'
+    });
+    file.bundle = new Bundle({
+      name: 'vendor',
+      type: BundleType.SCRIPTS
+    });
+    var expected = {
+      default: {
+        contents: {
+          main: {
+            scripts: "<script src='main-bundle.js' type='text/javascript'></script>",
+            styles: "<link href='main-bundle.css' media='screen' rel='stylesheet' type='text/css'/>"
+          },
+          vendor: {
+            scripts: "<script src='vendor-bundle.js' type='text/javascript'></script>"
+          }
+        },
+        filename: 'bundle.result.json'
+      }
+    };
+    addBundleResults(currentBundleResults, file).should.eql(expected);
+  });
+});
