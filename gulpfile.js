a1f11c758a12843a57cbde0cfb00688f63c1d950
Chris Montgomery <christopher.montgomery@dowjones.com>
2014-08-06 09:41:39 -0500

    add istanbul test coverage

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,13 @@ var gulp = require('gulp'),
   jshint = require('gulp-jshint'),
   path = require('path'),
   nicePackage = require('gulp-nice-package'),
-  shrinkwrap = require('gulp-shrinkwrap');
+  shrinkwrap = require('gulp-shrinkwrap'),
+  istanbul = require('gulp-istanbul'),
+  coverageEnforcer = require('gulp-istanbul-enforcer'),
+  TEST_FILES = [
+    './test/unit/**/*.js',
+    './test/integ/**/*.js'
+  ];
 
 require('gulp-help')(gulp);
 
@@ -24,21 +30,44 @@ gulp.task('shrinkwrap', 'Cleans package.json deps and generates npm-shrinkwrap.j
 gulp.task('lint', 'Lint all js', function () {
   return gulp.src([
     './*.js',
-    './lib/**/*.js',
-    './test/integ/**/*.js',
-    './test/unit/**/*.js'
-  ])
+    './lib/**/*.js'
+  ].concat(TEST_FILES))
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish', { verbose: true }))
     .pipe(jshint.reporter('fail'));
 });
 
 gulp.task('test', 'Tests', function () {
-  return gulp.src([
-    './test/unit/**/*.js',
-    './test/integ/**/*.js'
-  ])
+  return gulp.src(TEST_FILES)
     .pipe(mocha({reporter: 'dot'}));
+});
+
+gulp.task('test-cover', 'Unit tests and coverage', function (cb) {
+  gulp.src([
+    './index.js',
+    './lib/**/*.js'
+  ])
+    .pipe(istanbul())
+    .on('finish', function () {
+      gulp.src(TEST_FILES)
+        .pipe(mocha())
+        .pipe(istanbul.writeReports())
+        .on('end', function() {
+          var options = {
+            thresholds : {
+              statements : 96,
+              branches : 94,
+              lines : 97,
+              functions : 100
+            },
+            coverageDirectory : 'coverage',
+            rootDirectory : ''
+          };
+          return gulp.src('.')
+            .pipe(coverageEnforcer(options))
+            .on('end', cb);
+        });
+    });
 });
 
 gulp.task('test-debug', 'Run unit tests in debug mode', function () {
@@ -57,4 +86,4 @@ gulp.task('watch', 'Watch files and test on change', function () {
   ], ['test']);
 });
 
-gulp.task('ci', 'Runs all ci validation checks', ['lint', 'test', 'nice-package']);
+gulp.task('ci', 'Runs all ci validation checks', ['lint', 'test-cover', 'nice-package']);
