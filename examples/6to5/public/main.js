4340a1013dad5ea0ff03c2aace18cf1b8c365242
Chris Montgomery <christopher.montgomery@dowjones.com>
2014-12-01 10:01:35 -0600

    update example results after dep bump

--- examples/6to5/public/main.js
+++ examples/6to5/public/main.js
@@ -1,34 +1,36 @@
-var Test = function() {
-  function Test() {
+"use strict";
+
+var _classProps = function (child, staticProps, instanceProps) {
+  if (staticProps) Object.defineProperties(child, staticProps);
+  if (instanceProps) Object.defineProperties(child.prototype, instanceProps);
+};
+
+var Test = (function () {
+  var Test = function Test() {
     this.state = "test";
-  }
-
-  Test.prototype.printName = function(name) {
-    if (name === undefined)
-      name = "Colin";
-
-    var nameBlock = "John";
-
-    if(true){
-      var name;
-
-      (function() {
-        name = "Max";
-        var nameBlock = "Min";
-
-        //Max
-        console.log(name);
-        //Min
-        console.log(nameBlock);
-      })();
-    }
-
-    // Max
-    console.log(name);
-    // John
-    console.log(nameBlock);
   };
 
+  _classProps(Test, null, {
+    printName: {
+      writable: true,
+      value: function (name) {
+        if (name === undefined) name = "Colin";
+        var nameBlock = "John";
+
+        if (true) {
+          var name = "Max";
+          var _nameBlock = "Min";
+
+          console.log(name); //Max
+          console.log(_nameBlock); //Min
+        }
+
+        console.log(name); // Max
+        console.log(nameBlock); // John
+      }
+    }
+  });
+
   return Test;
-}();
+})();
 //# sourceMappingURL=maps/main.js.map
