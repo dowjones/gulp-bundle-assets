99c4b3b75f49619bbdb57ecd0847db09ac8e6d19
Chris Montgomery <chr.montgomery@gmail.com>
2015-02-04 16:31:29 -0600

    bump all example dep versions

--- bin/bumpDepsAllExamples.sh
+++ bin/bumpDepsAllExamples.sh
@@ -2,5 +2,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 for path in ${DIR}/../examples/*/; do
     echo "$path"
-    (cd "$path" && npm-check-updates -u)
+    (cd "$path" && npm-check-updates -u && rm -r node_modules && npm install)
 done
