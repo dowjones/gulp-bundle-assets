6655f4ed16d8ccb2e15a76c5519eb635f285e974
Chris Montgomery <chr.montgomery@gmail.com>
2016-05-06 08:20:32 -0500

    update to use gulp-clean-css instead of deprecated gulp-minify-css module

--- test/unit/stream-files-test.js
+++ test/unit/stream-files-test.js
@@ -36,7 +36,7 @@ describe('stream-files', function () {
               styles: transformHelper.less()
             },
             pluginOptions: {
-              'gulp-minify-css': {},
+              'gulp-clean-css': {},
               'gulp-uglify': {},
               'gulp-concat': {},
               'gulp-sourcemaps': {'scripts': {}, 'styles': {}}
@@ -73,7 +73,7 @@ describe('stream-files', function () {
               scripts: transformHelper.coffee()
             },
             pluginOptions: {
-              'gulp-minify-css': {},
+              'gulp-clean-css': {},
               'gulp-uglify': {},
               'gulp-concat': {},
               'gulp-sourcemaps': {'scripts': {}, 'styles': {}}
