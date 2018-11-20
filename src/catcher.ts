77426dee80b53d8d68514fb8fc9b097d8f483252
Jordan Mele <SiliconSoldier@outlook.com.au>
2018-11-20 17:28:03 +1100

    Fixed results being sent back far too soon and missing types attribute on package

--- src/catcher.ts
+++ src/catcher.ts
@@ -1,8 +1,8 @@
 import { Transform, TransformCallback } from "stream";
-import LazyPromise from "p-lazy";
 
 /**
  * All this does is collect all stream data and once all read resolves a promise with the collected chunks.
+ * TODO Handle when the stream source has no chunks to pass
  */
 export class Catcher extends Transform {
     /**
@@ -14,22 +14,28 @@ export class Catcher extends Transform {
      * Holds caught stream content.
      */
     private Results: any[] = [];
+
+    /**
+     * Resolver for promise, may not be immeditately set.
+     */
+    private Resolve?: (value?: any[] | PromiseLike<any[]>) => void;
     
     constructor() {
         super({
             objectMode: true
         });
 
-        this.Collected = new LazyPromise<any[]>(resolve => {
-            resolve(this.Results);
+        // Set promise
+        this.Collected = new Promise<any[]>(resolve => {
+            this.Resolve = resolve;
         });
     }
 
     /**
      * Collects incoming chunks.
-     * @param chunk 
-     * @param encoding 
-     * @param callback 
+     * @param chunk Incoming chunk to catch.
+     * @param encoding Its encoding, if applicable.
+     * @param callback Callback used to indicate method completion.
      */
     _transform(chunk: any, encoding: string, callback: TransformCallback): void {
         this.Results.push(chunk);
@@ -37,11 +43,19 @@ export class Catcher extends Transform {
     }
 
     /**
-     * Triggers lazy promise to return final results.
-     * @param callback 
+     * Resolves collection promise.
+     * @param callback Callback used to indicate method completion.
      */
     _flush(callback: TransformCallback): void {
-        // Probe lazy-promise to trigger call chain
-        this.Collected.then(() => callback());
+        const resolver = () => {
+            if (this.Resolve) {
+                this.Resolve(this.Results);
+                callback();
+            }
+            else {
+                setTimeout(resolver, 5);
+            }
+        };
+        resolver();
     }
 }
